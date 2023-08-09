a2048908c1bcb86657efb3138ed4806294ad6c88
TARUNES <tarunes12@gmail.com>
2023-08-09 16:05:27 +0530

    conflict2

--- src/components/pageComponents/Topbar/Topbar.jsx
+++ src/components/pageComponents/Topbar/Topbar.jsx
@@ -28,7 +28,7 @@ export default function Topbar() {
             <Link href="/">CONTACT</Link>
           </li>
           <li className="topListItem">
-            <Link href="/YourProduct">MyProducts</Link>
+            <Link href="/YourProduct">MY PRODUCTS</Link>
           </li>
         </ul>
       </div>
