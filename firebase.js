34adbc88721832f3096ecdff9c4133627b855fbd
TARUNES <tarunes12@gmail.com>
2023-08-09 15:52:20 +0530

    Revert "inum cart matum thaan balance"
    This reverts commit f14fc5315f43e04a6f5d169facabb31149071386.

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-// import firebase from 'firebase/app';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- src/app/(yourProduct)/YourProduct/page.js
+++ src/app/(yourProduct)/YourProduct/page.js
@@ -2,12 +2,10 @@
 
 import React, { useEffect, useState } from "react";
 import { Addcard } from "@/components/ui/addcard";
-import { collection, getDocs, where, query } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../../firebase";
 import Topbar from "@/components/pageComponents/Topbar/Topbar";
 import { Adddelcard } from "@/components/ui/addcarddel";
-import { auth } from "../../../../firebase";
-import { Button } from "@/components/ui/button";
 
 const YourProduct = () => {
   const [productList, setproductList] = useState([]);
@@ -16,16 +14,11 @@ const YourProduct = () => {
   useEffect(() => {
     const getProducts = async () => {
       const ProductCollection = collection(db, "products");
-      // const userProductsQuery = query(
-      //   ProductCollection,
-      //   where("userID", "==", auth.currentUser?.uid)
-      // );
       const ProductSnapshot = await getDocs(ProductCollection);
       const prod = ProductSnapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
       }));
-
       setproductList(prod);
       setLoading(false);
       console.log(productList);
@@ -36,18 +29,15 @@ const YourProduct = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  console.log(auth.currentUser?.email);
+
   return (
     <div>
-      <h1>{auth.currentUser?.uid}</h1>
+      <h1>cards</h1>
       <div>
         {productList.map((item) => {
-          if (item.userId == auth.currentUser?.uid) {
-            return <Adddelcard props={item}></Adddelcard>;
-          }
+          return <Adddelcard props={item}></Adddelcard>;
         })}
       </div>
-      <button onClick={() => auth.signOut()}>Log out</button>
     </div>
   );
 };
